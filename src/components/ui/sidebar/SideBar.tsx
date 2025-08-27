04e3ddba1b688d5dbda04e409048e7f6bddcb526
karelycode <20041240@itdurango.edu.mx>
2025-08-27 16:07:31 -0600

    feat: Add SideBar component and integrate with TopMenu; update package dependencies

---
+++ src/components/ui/sidebar/SideBar.tsx
@@ -0,0 +1,123 @@
+"use client";
+import { useUIStore } from "@/store";
+import Link from "next/link";
+import React from "react";
+import clsx from "clsx";
+import {
+  IoCloseOutline,
+  IoLogInOutline,
+  IoLogOutOutline,
+  IoPeopleOutline,
+  IoPersonOutline,
+  IoSearchOutline,
+  IoShirtOutline,
+  IoTicketOutline,
+} from "react-icons/io5";
+
+export const SideBar = () => {
+  const isSideMenuOpen = useUIStore((state) => state.isSideMenuOpen);
+  const closeSideMenu = useUIStore((state) => state.closeSideMenu);
+
+  return (
+    <div>
+      {/*Background black*/}
+      {isSideMenuOpen && (
+        <div className="fixed top-0 left-0 w-screen h-screen bg-black opacity-30" />
+      )}
+
+      {/*Blur*/}
+      {isSideMenuOpen && (
+        <div
+          onClick={closeSideMenu}
+          className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-blur-md backdrop-filter"
+        />
+      )}
+
+      {/*Sidemenu*/}
+
+      <nav
+        className={clsx(
+          "fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300",
+          {
+            "translate-x-full": !isSideMenuOpen,
+          }
+        )}
+      >
+        <IoCloseOutline
+          size={50}
+          className="absolute top-5 right-5 cursor-pointer"
+          onClick={closeSideMenu}
+        />
+        {/* Input */}
+        <div className="relative mt-14">
+          <IoSearchOutline size={20} className="absolute top-2 left-2" />
+          <input
+            type="text"
+            placeholder="Buscar..."
+            className="w-full  bg-gray-50 rounded py-1 pl-10 pr-10 border-b-2 text-xl border-gray-200 focus:outline-none  focus:border-blue-500"
+          />
+        </div>
+
+        {/*Menu Items */}
+        <Link
+          href="/"
+          className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+          <IoPersonOutline size={30} />
+          <span className="ml-3 text-xl">Perfil</span>
+        </Link>
+
+        <Link
+          href="/"
+          className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+          <IoTicketOutline size={30} />
+          <span className="ml-3 text-xl">Ordenes</span>
+        </Link>
+
+        <Link
+          href="/"
+          className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+          <IoLogInOutline size={30} />
+          <span className="ml-3 text-xl">Iniciar Sesión</span>
+        </Link>
+
+        <Link
+          href="/"
+          className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+          <IoLogOutOutline size={30} />
+          <span className="ml-3 text-xl">Cerrar Sesión</span>
+        </Link>
+
+        {/*Line separator*/}
+        <div className="w-full h-px bg-gray-200 my-10" />
+
+        <Link
+          href="/"
+          className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+          <IoShirtOutline size={30} />
+          <span className="ml-3 text-xl">Productos</span>
+        </Link>
+
+        <Link
+          href="/"
+          className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+          <IoTicketOutline size={30} />
+          <span className="ml-3 text-xl">Ordenes</span>
+        </Link>
+
+        <Link
+          href="/"
+          className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+          <IoPeopleOutline size={30} />
+          <span className="ml-3 text-xl">Usuarios</span>
+        </Link>
+      </nav>
+    </div>
+  );
+};
--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -4,8 +4,10 @@ import Link from "next/link";
 import { IoSearchOutline, IoCartOutline } from "react-icons/io5";
 
 import { titleFont } from "@/config/fonts";
+import { useUIStore } from "@/store/ui/ui-store";
 
 export const TopMenu = () => {
+  const openSideMenu = useUIStore((state) => state.openSideMenu);
   return (
     <nav className="bg-white flex px-5 justify-between items-center w-full">
       <div className="text-lg font-bold">
@@ -52,7 +54,10 @@ export const TopMenu = () => {
             <IoCartOutline className="w-5 h-5 " />
           </div>
         </Link>
-        <button className="m-2 p-2 rounded-md transition-all hover:bg-gray-200">
+        <button
+          onClick={openSideMenu}
+          className="m-2 p-2 rounded-md transition-all hover:bg-gray-200"
+        >
           Menu
         </button>
       </div>
