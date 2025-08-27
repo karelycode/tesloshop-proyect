1d2f1acd29dcd935a7a48b01fb11ca20ba13752d
karelycode <20041240@itdurango.edu.mx>
2025-08-27 00:19:19 -0600

    Add new product images and implement 404 page not found component
    - Added multiple new product images to the public/products directory.
    - Updated the category page to handle "kids" category with a not found response.
    - Created a new not-found page component to display a user-friendly 404 error message.
    - Exported the new PageNotFound component from the components index file.

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,7 +1,20 @@
-export default function () {
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default function ({ params }: Props) {
+  const { id } = params;
+  if (id === "kids") {
+    notFound();
+  }
+
   return (
     <div>
-      <h1>Category page</h1>
+      <h1>Category page: {id}</h1>
     </div>
   );
 }
---
+++ src/components/ui/not-found/PageNotFound.tsx
@@ -0,0 +1,38 @@
+import { titleFont } from "@/config/fonts";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export const PageNotFound = () => {
+  return (
+    <div className="flex flex-col-reverse md:flex-row h-[800px] w-full justify-center items-center align-middle">
+      <div className="text-center px-5 mx-5">
+        <h1 className={`${titleFont.className} text-5xl font-bold`}>
+          404 - Page Not Found
+        </h1>
+        <p className="font-semibold text-xl">Whoops! Lo sentimos mucho</p>
+        <p className="font-light">
+          <span>
+            Puedes regresar al{" "}
+            <Link
+              href="/"
+              className="font-normal hover:underline transition-all"
+            >
+              inicio
+            </Link>
+          </span>
+        </p>
+      </div>
+
+      <div className="px-5 mx-5">
+        <Image
+          src="/imgs/starman_750x750.png"
+          alt="Not Found"
+          className="p-5 sm:p-0"
+          width={550}
+          height={550}
+        />
+      </div>
+    </div>
+  );
+};
