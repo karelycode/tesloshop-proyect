2128886f15c33d8a3aeb8a14ad853b5f885f1857
karelycode <20041240@itdurango.edu.mx>
2025-08-26 23:09:38 -0600

    feat: Add react-icons dependency and update TopMenu with search and cart icons style: Change background color of TopMenu to white fix: Update globals.css to use @import for Tailwind CSS

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -1,12 +1,13 @@
 "use client";
 import React from "react";
 import Link from "next/link";
+import { IoSearchOutline, IoCartOutline } from "react-icons/io5";
 
 import { titleFont } from "@/config/fonts";
 
 export const TopMenu = () => {
   return (
-    <nav className="bg-yellow-400 flex px-5 justify-between items-center w-full">
+    <nav className="bg-white flex px-5 justify-between items-center w-full">
       <div className="text-lg font-bold">
         <Link href="/">
           <span className={`${titleFont.className} antialiased font-bold`}>
@@ -37,6 +38,23 @@ export const TopMenu = () => {
         >
           Niños
         </Link>
+      </div>
+
+      <div className="flex items-center">
+        <Link href="/search" className="mx-2">
+          <IoSearchOutline className="w-5 h-5" />
+        </Link>
+        <Link href="/cart" className="mx-2">
+          <div className="relative">
+            <span className="absolute text-xs rounded-full px-1 font-bold -top-2 bg-blue-700 text-white">
+              3
+            </span>
+            <IoCartOutline className="w-5 h-5 " />
+          </div>
+        </Link>
+        <button className="m-2 p-2 rounded-md transition-all hover:bg-gray-200">
+          Menu
+        </button>
       </div>
     </nav>
   );
