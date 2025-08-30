84056c87cc07ca1010ec9a0ddfe4b346471cac9e
karelycode <20041240@itdurango.edu.mx>
2025-08-29 18:51:00 -0600

    feat: Add SizeSelector component and integrate into product page; implement not found page

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,5 @@ export * from "./ui/title/Title";
 export * from "./products/product-grid/ProductGrid";
 export * from "./products/product-grid/ProductGridItem";
 export * from "./ui/sidebar/SideBar";
+
+export * from "./product/size-selector/SizeSelector";
