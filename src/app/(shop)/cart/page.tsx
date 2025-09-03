69ae49eec3f14d4ca4f732163ef5d32d695e148d
karelycode <20041240@itdurango.edu.mx>
2025-09-02 20:24:49 -0600

    feat: Update cart page to display products with images and quantity selector; enhance checkout summary

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,7 +1,82 @@
+import { Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import Link from "next/link";
+import Image from "next/image";
+import { QuantitySelector } from "../../../components/product/quantity-selector/QuantitySelector";
+
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
 export default function () {
   return (
-    <div>
-      <h1>Cart page</h1>
+    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+      <div className="flex flex-col w-[1000px]">
+        <Title title="Carrito" />
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          {/* Carrito */}
+
+          <div className="flex flex-col mt-5 ">
+            <span className="text-xl">Agregar más productos</span>
+            <Link href="/" className="underline mb-5">
+              Continuar comprando
+            </Link>
+
+            {/* Items */}
+            {productsInCart.map((product) => (
+              <div key={product.slug} className="flex mb-5">
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  alt={product.title}
+                  width={100}
+                  height={100}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                  }}
+                  className="mr-5 rounded"
+                />
+
+                <div>
+                  <p>{product.title}</p>
+                  <p>${product.price}</p>
+                  <QuantitySelector quantity={1} />
+
+                  <button className="underline mt-3">Eliminar</button>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {/* Checkout */}
+          <div className="bg-white rounded-xl shadow-xl p-7">
+            <h2 className="text-2xl mb-2">Resumen de compra</h2>
+            <div className="grid grid-cols-2 ">
+              <span>No. Productos</span>
+              <span className="text-right">3 articulos</span>
+
+              <span>Subtotal</span>
+              <span className="text-right">$300</span>
+
+              <span>Impuestos (15%)</span>
+              <span className="text-right">$45</span>
+
+              <span className="text-2xl mt-5">Total</span>
+              <span className="text-right text-2xl mt-5">$345</span>
+            </div>
+            <div className="mt-5 mb-2 w-full">
+              <Link
+                href="/checkout/address"
+                className="flex btn-primary justify-center"
+              >
+                Ir a pagar
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
