9b92ac6eb798c20847988b6cb779dd428c8bc428
karelycode <20041240@itdurango.edu.mx>
2025-09-02 21:57:27 -0600

    feat: Implement redirect functionality for empty cart and enhance empty cart page layout

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -3,6 +3,7 @@ import { initialData } from "@/seed/seed";
 import Link from "next/link";
 import Image from "next/image";
 import { QuantitySelector } from "../../../components/product/quantity-selector/QuantitySelector";
+import { redirect } from "next/navigation";
 
 const productsInCart = [
   initialData.products[0],
@@ -11,6 +12,8 @@ const productsInCart = [
 ];
 
 export default function () {
+  //redirect("/empty");
+
   return (
     <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
       <div className="flex flex-col w-[1000px]">
--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+import { IoCartOutline } from "react-icons/io5";
+
 export default function () {
   return (
-    <div>
-      <h1>Cart page</h1>
+    <div className="flex justify-center items-center h-[800px]">
+      <IoCartOutline size={100} className="mx-5" />
+      <div>
+        <h1 className="text-xl font-semibold">Tu carrito está vacío</h1>
+        <Link href="/" className="text-blue-500 text-4xl mt-2">
+          Ir a la tienda
+        </Link>
+      </div>
     </div>
   );
 }
